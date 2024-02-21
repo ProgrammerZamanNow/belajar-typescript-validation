f9b946a41e799e85df2dc1e995dadbb3b4a881f5
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2024-02-21 22:32:22 +0700

    Add Zod error testing
    The commit adds two new tests to the validation suite which specifically examine Zod library error handling. The error examination includes testing for valid email strings and Zod's error responses for invalid inputs.

--- test/validation.test.ts
+++ test/validation.test.ts
@@ -1,4 +1,4 @@
-import {z} from "zod";
+import {z, ZodError} from "zod";
 
 describe('zod', () => {
 
@@ -56,6 +56,37 @@ describe('zod', () => {
 
         const birthDate2 = birthDateSchema.parse(new Date(1990, 0, 1));
         console.info(birthDate2);
+
+    });
+
+    it('should return zod error if invalid', async () => {
+
+        const schema = z.string().email().min(3).max(100);
+
+        try {
+            schema.parse("ek");
+        }catch (err){
+            if (err instanceof ZodError){
+                console.error(err);
+                // err.errors.forEach((error) => {
+                //     console.info(error.message);
+                // })
+            }
+        }
+
+    });
+
+    it('should return zod error if invalid without exception', async () => {
+
+        const schema = z.string().email().min(3).max(100);
+
+        const result = schema.safeParse("eko@example.com");
+
+        if(result.success){
+            console.info(result.data);
+        } else {
+            console.error(result.error);
+        }
 
     });
 
