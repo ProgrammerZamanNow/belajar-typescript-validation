1584a43f740d73337cd20c1fef01e33ec79edd6a
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2024-02-21 22:24:41 +0700

    Add date validation test
    This commit introduces a new test that focuses on date validation functionality. The validation is performed using the zod library, covering different date formats and validating date range constraints.

--- test/validation.test.ts
+++ test/validation.test.ts
@@ -47,4 +47,16 @@ describe('zod', () => {
 
     });
 
+    it('should support date validation', async () => {
+
+        const birthDateSchema = z.coerce.date().min(new Date(1980, 0, 1)).max(new Date(2020, 0, 1));
+
+        const birthDate = birthDateSchema.parse("1990-01-01");
+        console.info(birthDate);
+
+        const birthDate2 = birthDateSchema.parse(new Date(1990, 0, 1));
+        console.info(birthDate2);
+
+    });
+
 });
