26e8206e380a02c4cf325f609fb5e36e64fa5485
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2024-02-21 22:53:16 +0700

    Add object validation test with custom message
    This commit introduces a new test that checks the validation of objects with custom error messages. The test uses the Zod library to create a schema for a login object and check whether values entered for username and password fit within stated constraints. The custom error messages are specifically tested when these constraints are not met.

--- test/validation.test.ts
+++ test/validation.test.ts
@@ -180,4 +180,25 @@ describe('zod', () => {
 
     });
 
+    it('should can validate object with message', async () => {
+
+        const loginSchema = z.object({
+            username: z.string().email("username harus email"),
+            password: z.string().min(6, "password min harus 6 karakter").max(20, "password max harus 20 karakter")
+        });
+
+        const request = {
+            username: "eko",
+            password: "12",
+        };
+
+        try {
+            const result = loginSchema.parse(request);
+            console.info(result);
+        }catch (err){
+            console.error(err);
+        }
+
+    });
+
 });
