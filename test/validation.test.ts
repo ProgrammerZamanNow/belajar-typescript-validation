5ba9eac9245e8ccc86d013248b01722f9ba18b97
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2024-02-21 22:48:24 +0700

    Add array, set, and map validation tests
    This commit introduces three new tests, each focusing on a specific data structure: arrays, sets, and maps. They all test email-based validation using the Zod library. These tests ensure that the Zod validation works appropriately with these data structures, handling minimum and maximum values correctly.

--- test/validation.test.ts
+++ test/validation.test.ts
@@ -138,4 +138,46 @@ describe('zod', () => {
 
     });
 
+    it('should support array validation', async () => {
+
+        const schema = z.array(
+            z.string().email()
+        ).min(1).max(10);
+
+        const request: Array<string> = ["eko@example.com", "budi@example.com"];
+        const result: Array<string> = schema.parse(request);
+
+        console.info(result);
+
+    });
+
+    it('should support set validation', async () => {
+
+        const schema = z.set(
+            z.string().email()
+        ).min(1).max(10);
+
+        const request: Set<string> = new Set(["eko@example.com", "budi@example.com", "eko@example.com"]);
+        const result: Set<string> = schema.parse(request);
+
+        console.info(result);
+
+    });
+
+    it('should support map validation', async () => {
+
+        const schema = z.map(
+            z.string(),
+            z.string().email()
+        );
+
+        const request: Map<string, string> = new Map([
+            ["eko", "eko@example.com"],
+            ["budi", "budi@example.com"],
+        ]);
+        const result: Map<string, string> = schema.parse(request);
+        console.info(result);
+
+    });
+
 });
